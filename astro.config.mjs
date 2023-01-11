c8ef085ed21faca72c0a6f01c7ffec04550e83a6
Manuel Ernesto Garcia <merodriguez9112@gmail.com>
2023-01-10 23:26:52 -0500

    adding workflow for github page build

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,5 +8,6 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://cucoderscommunity.github.io',
   integrations: [tailwind(), sitemap()]
 });
