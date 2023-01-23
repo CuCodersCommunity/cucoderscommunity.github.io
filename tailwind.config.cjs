968d136d63aedc66892aab11deaf7bf556f0825d
Manuel Ernesto Garcia <merodriguez9112@gmail.com>
2023-01-23 13:48:51 -0500

    improving members directory

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,10 +1,9 @@
 /** @type {import('tailwindcss').Config} */
+
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}', "./node_modules/flowbite/**/*.js"],
-	theme: {
-		extend: {},
-	},
-	plugins: [require('flowbite/plugin'), require('@tailwindcss/typography')],
-}
-
-
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}", "./node_modules/flowbite/**/*.js"],
+  theme: {
+    extend: {},
+  },
+  plugins: [require("flowbite/plugin"), require("@tailwindcss/typography"), require('@tailwindcss/line-clamp')],
+};
