568917cfe673bcea1ac7abb0ffaa4e48a0173a5e
Manuel Ernesto Garcia <merodriguez9112@gmail.com>
2023-02-05 12:42:41 -0500

    creating apps directory routes and views

--- src/content/config.ts
+++ src/content/config.ts
@@ -9,10 +9,22 @@ const blogSchema = z.object({
     categories: z.array(z.string()),
 });
 
+const appsSchema = z.object({
+    title: z.string(),
+    username: z.string(),
+    pubDate: z.string().transform(str => new Date(str)),
+    description: z.string().optional(),
+    image: z.string().optional(),
+    categories: z.array(z.string()),
+});
+
 export type BlogSchema = z.infer<typeof blogSchema>;
+export type AppSchema = z.infer<typeof appsSchema>;
 
 const blogCollection = defineCollection({ schema: blogSchema });
+const appsCollection = defineCollection({ schema: appsSchema });
 
 export const collections = {
     'blog': blogCollection,
+    'apps': appsCollection
 }
