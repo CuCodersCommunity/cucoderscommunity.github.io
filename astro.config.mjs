1f7f17b7be2c98a7e05dcbeee7167ef5330b9506
Manuel Ernesto Garcia <merodriguez9112@gmail.com>
2023-02-23 17:22:57 -0500

    testing build changing sit url to new domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,6 +8,6 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://cucoderscommunity.github.io/',
+  site: 'https://www.cucoders.dev/',
   integrations: [tailwind(), sitemap()]
 });
