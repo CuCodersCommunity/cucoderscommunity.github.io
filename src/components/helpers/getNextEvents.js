10a0c0933ae5b37eb4cece7538c6be91885e9a2d
Manuel Ernesto Garcia <merodriguez9112@gmail.com>
2023-05-03 17:09:48 -0400

    handling events with no end date

--- src/components/helpers/getNextEvents.js
+++ src/components/helpers/getNextEvents.js
@@ -7,7 +7,13 @@ dayjs.locale("es");
 async function getNextEvents(url, type = "json") {
   const allEvents = await getCollection("events");
   let filterEvents = await allEvents
-    .filter((event) => event.data.endDate >= new Date())
+    .filter((event) => {
+      if (isNaN(event.data.endDate.getTime())) {
+        return event.data.startDate >= new Date();
+      } else {
+        return event.data.endDate >= new Date();
+      }
+    })
     .sort((a, b) => a.data.startDate.valueOf() - b.data.startDate.valueOf())
     .slice(0, 3);
 
@@ -15,10 +21,9 @@ async function getNextEvents(url, type = "json") {
 
   filterEvents.forEach((event) => {
     let display_date = `${dayjs(event.data.startDate).format("D [de] MMMM")}`;
-    if (!dayjs(event.data.startDate).isSame(event.data.endDate, "day")) {
+    if (!isNaN(event.data.endDate.getTime()) && !dayjs(event.data.startDate).isSame(event.data.endDate, "day")) {
       display_date = display_date + ` - ${dayjs(event.data.endDate).format("D [de] MMMM")}`;
     }
-
     nextEvents.push({
       name: event.data.name,
       date: display_date,
