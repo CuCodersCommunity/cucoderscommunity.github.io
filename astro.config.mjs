6a4055196e1cbb3887b315702979203a0506ca58
Manuel Ernesto Garcia <merodriguez9112@gmail.com>
2023-05-10 21:42:29 -0400

    adding last mod to jobs in sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -15,9 +15,22 @@ import image from "@astrojs/image";
 // https://astro.build/config
 export default defineConfig({
   site: "https://cucoders.dev/",
-  integrations: [tailwind(), sitemap(), partytown({
-    config: {
-      forward: ["dataLayer.push"]
-    }
-  }), image()]
+  integrations: [
+    tailwind(),
+    sitemap({
+      serialize(item) {
+        if (/.*empleos\/(\d{4}-\d{2}-\d{2})\/.*/.test(item.url)) {
+          const date = item.url.split("/")[4]
+          item.lastmod = new Date(date);
+        }
+        return item;
+      },
+    }),
+    partytown({
+      config: {
+        forward: ["dataLayer.push"],
+      },
+    }),
+    image(),
+  ],
 });
